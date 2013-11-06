2b69f3ce43f737083d4dc9f6638c41e9b826f900
Eric Gorr <ericgorr@mac.com>
2013-11-05 21:09:40 -0500

    initial commit

--- search test/ELIZAppDelegate.m
+++ search test/ELIZAppDelegate.m
@@ -12,7 +12,33 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-	// Insert code here to initialize your application
+	NSBundle* mainBundle = [NSBundle mainBundle];
+	
+	NSURL*		docURL				= [mainBundle URLForResource:@"message" withExtension:@"emlx"];
+	NSString*	homePath			= NSHomeDirectory();
+	NSURL*		homeURL				= [NSURL fileURLWithPath:homePath];
+	NSString*   searchIndexName     = @"index.idx";
+	NSURL*		indexURL			= [homeURL URLByAppendingPathComponent:searchIndexName];
+	
+	SKIndexRef  searchIndexFile;
+	
+    SKLoadDefaultExtractorPlugIns();
+    
+    NSDictionary*   textAnalysisOptions = @{ (__bridge NSString*)kSKProximityIndexing : @YES };
+    
+    [[NSFileManager defaultManager] removeItemAtURL:indexURL error:nil];
+    
+    searchIndexFile = SKIndexCreateWithURL( (__bridge CFURLRef)indexURL, (__bridge CFStringRef)searchIndexName, kSKIndexInverted, (__bridge CFDictionaryRef)textAnalysisOptions );
+
+    NSLog( @"Index Exists: %d", [[NSFileManager defaultManager] fileExistsAtPath:[indexURL path]] );
+    NSLog( @"Message Exists: %d", [[NSFileManager defaultManager] fileExistsAtPath:[docURL path]] );
+    
+    CFURLRef        fullMessageURLRef           = (CFURLRef)CFBridgingRetain( docURL );
+    SKDocumentRef   doc                         = SKDocumentCreateWithURL ( fullMessageURLRef );
+    
+    SKIndexAddDocument( searchIndexFile, doc, NULL, false );
+    
+    NSLog( @"added" );
 }
 
 @end
